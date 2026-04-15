4cb5ca80914ca52fb7a9a5bbd541a7b8c7b46e5b
Lizhiwei415221 <193952040@qq.com>
2026-04-15 19:37:56 +0800

    Update api.php

--- equipment/routes/api.php
+++ equipment/routes/api.php
@@ -4,21 +4,19 @@ use App\Http\Controllers\LXController;
 use App\Http\Controllers\LZWController;
 use Illuminate\Support\Facades\Route;
 
-// 公开接口（不需要token）
-// 用户注册
-Route::post('/register', [LZWController::class, 'register']);
-//用户登录
-Route::post('/login', [LZWController::class, 'login']);
+// ======================
+// 用户认证接口
+// ======================
+Route::post('/auth/register', [LZWController::class, 'register']);
+Route::post('/auth/login', [LZWController::class, 'login']);
 
-// 需要登录认证的接口
+// 需要登录
 Route::middleware('auth:sanctum')->group(function () {
-    //获取当前用户
-    Route::get('/me', [LZWController::class, 'me']);
-    // 退出登录
-    Route::post('/logout', [LZWController::class, 'logout']);
-    // 示例：只有管理员能查看所有用户
-    Route::get('/admin/users', [LZWController::class, 'adminUsers'])
-        ->middleware('auth:sanctum', 'check.admin');//
+    Route::get('/auth/me', [LZWController::class, 'me']);
+    Route::post('/auth/logout', [LZWController::class, 'logout']);
+
+    // 管理员接口
+    Route::get('/admin/users', [LZWController::class, 'adminUsers']);
 });
 
 
