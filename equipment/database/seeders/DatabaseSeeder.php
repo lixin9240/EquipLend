951249c516117a6e46a734a03665530573632b69
lixin9240 <3258599349@qq.com>
2026-04-16 19:17:48 +0800

    Update DatabaseSeeder.php

--- equipment/database/seeders/DatabaseSeeder.php
+++ equipment/database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder
          // 创建管理员账号2--LZW
         User::create([
             'account' => 'admin124',
-            'name' => '系统管理员',
+            'name' => '系统管理员1',
             'password' => 'admin124',  // 会自动哈希加密
             'role' => 'admin',
             'email' => '193952040@qq.com',
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
          // 创建管理员账号--WLJ
         User::create([
             'account' => 'admin125',
-            'name' => '系统管理员',
+            'name' => '系统管理员2',
             'password' => 'admin125',  // 会自动哈希加密
             'role' => 'admin',
             'email' => '2633681826@qq.com',
