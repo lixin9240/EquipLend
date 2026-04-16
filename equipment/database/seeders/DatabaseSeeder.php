79101b74f1db5523c9c8abbc4302496e9b3b7703
lixin9240 <3258599349@qq.com>
2026-04-16 19:08:32 +0800

    Update DatabaseSeeder.php

--- equipment/database/seeders/DatabaseSeeder.php
+++ equipment/database/seeders/DatabaseSeeder.php
@@ -16,15 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // 创建管理员账号
+        // 创建管理员账号1--LX
         User::create([
             'account' => 'admin123',
             'name' => '系统管理员',
-            'password' => 'admin123',  // 会自动哈希加密
+            'password' => 'admin124',  // 会自动哈希加密
             'role' => 'admin',
             'email' => '3258599349@qq.com',
         ]);
 
+         // 创建管理员账号2--LZW
+        User::create([
+            'account' => 'admin124',
+            'name' => '系统管理员',
+            'password' => 'admin124',  // 会自动哈希加密
+            'role' => 'admin',
+            'email' => '193952040@qq.com',
+        ]);
+
+         // 创建管理员账号--WLJ
+        User::create([
+            'account' => 'admin125',
+            'name' => '系统管理员',
+            'password' => 'admin125',  // 会自动哈希加密
+            'role' => 'admin',
+            'email' => '2633681826@qq.com',
+        ]);
         // 创建测试学生账号1
         User::create([
             'account' => '2021001',
@@ -34,13 +51,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'zhangsan@example.com',
         ]);
 
-        // 创建测试学生账号2
-        User::create([
-            'account' => '2021002',
-            'name' => '李四',
-            'password' => '123456',  // 会自动哈希加密
-            'role' => 'student',
-            'email' => 'lisi@example.com',
-        ]);
     }
 }
